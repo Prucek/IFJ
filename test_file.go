67554562c0b402c5af9f815ea7498cdcec9fb317
Peter <peter.rucek@gmail.com>
2020-11-20 11:04:48 +0100

    Add to test_file.go for finding errors

--- test_file.go
+++ test_file.go
@@ -33,6 +33,84 @@ func main(){
 
         a = 1 +5 * (1 / 5)
 }
+func main(){
+
+}
+
+func maine (i int, b string)(){
+
+        b := "Kurna"
+        b = "nyeeee"
+        b, i = "precooo", 44
+        ahoj := a
+        id := 5
+        ahoj, id = a, b
+        return ahoj
+        if a {
+                j :=1
+                if b {
+                        ahoj = a
+                        return ahoj
+                } else{
+                }
+                return ahoj
+        }else{
+                //id1 , id2 , id3,_ = a, b,c,d
+        }
+        //ahoj := b
+        ahoj(24) // here not reporting error
+        ahoj()
+        id = 25.3
+        return masci,aj
+}
+
+func ahoj(){
+    //ahoj = a
+    //i = 5
+    od := (5*8)/0
+    id := (od*2)/4+5 != 1 +9
+    len("ahoj")
+    id, od = maine(9,"hnusna skola")
+    id, od = dobry_den(9,"vysrata matika")
+    inputf(25.4)
+    inputf()
+    int2float(25, 45)
+
+        for ;a ;  {
+                if a{
+                        id = id*id
+                        for a:=0; a!= 3;a=a +1{
+                                return 7
+                        }
+                } else {
+                        //ahje = a
+                        b := 3
+                        //b := a
+                }
+                ahoj()
+                dobry_den("ahoj", 24)
+                preco(25, 4, 8.5)
+                main()
+
+                ///id, od = dobry_den("ahoj", 24.5)  //hadze syntakticku chybu vzdy ked priradujeme funkciu premennej, treba dorobit spravne express.
+                // takze nemozem poriadne otestovat ci sa zhoduje lava strana priradenia s poctom a navratov. hodnotami volanej funckie
+                
+                blablabla()
+        }
+    bebi := "hello"+"its"-"me"
+    id = 5
+}
+
+func dobry_den (s string, i int) (int,string){
+
+        dobry_den()
+        print("STRNIG =" ,s, "error =", code, "\n")
+        print(25, ":", "Ahoj")
+}
+
+func preco(i int, j int, k float64){
+
+}       
 
 func achoj(i int) (string) {
 
